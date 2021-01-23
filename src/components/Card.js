f4f2f0c7b7ce855d5a77f498ab734234c903d6a5
Luis Dufour <lgudinodufour@gmail.com>
2021-01-23 13:35:59 -0300

    se aplican props sobre el primer componente creado Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import exerciseIMG from '../images/exercise.png';
+import circlesIMG from '../images/circles.png';
 import './styles/Card.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Card extends React.Component{
+
     render(){
+        const {title, description, img, leftColor, rightColor} = this.props;
         return(
 
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card" 
+            
+            style={{
+                backgroundImage: `url(${circlesIMG}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+            }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exerciseIMG} className="float-right"/>
+                            <img src={img} className="float-right"/>
                         </div>
                         
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calisthenics</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
