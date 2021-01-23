9c6ba7781fbdf63c912c29cacf0d63babf6fed24
Luis Dufour <lgudinodufour@gmail.com>
2021-01-23 03:16:15 -0300

    se agregaron estilos y la dependencia de Bootstrap

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import exerciseIMG from '../images/exercise.png';
+import './styles/Card.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Card extends React.Component{
     render(){
         return(
-            <div>
-                <div>
-                    <img src={exerciseIMG}/>
-                </div>
-                
-                <div>
-                    <h1>Technique Guides</h1>
-                    <p>Learn amazing street workout and calisthenics</p>
+
+            <div className="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={exerciseIMG} className="float-right"/>
+                        </div>
+                        
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Technique Guides</h1>
+                            <p>Learn amazing street workout and calisthenics</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
